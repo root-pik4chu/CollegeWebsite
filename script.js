acbf981c7927e2ea56af12982beb2bd44c071c3d
pikachu <sahilsaundale@gmail.com>
2024-07-12 00:36:56 +0530

    now i am goid too edit again ..... lets see its7th design

--- script.js
+++ script.js
@@ -3,57 +3,74 @@
 
 
 
-
+// SwiperJs()
 reveal_Item();
 
-loco()
+// loco()
+
+
+
+lenis();
 
 
 
 
 
 
+function lenis(){
+   const lenis = new Lenis()
 
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
 
+lenis.on('scroll', ScrollTrigger.update)
 
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
 
-
-
-function loco(){
-   gsap.registerPlugin(ScrollTrigger);
-
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
-
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-
+gsap.ticker.lagSmoothing(0)
 }
 
- 
+// function loco(){
+//    gsap.registerPlugin(ScrollTrigger);
+
+// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+// const locoScroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true
+// });
+// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+// locoScroll.on("scroll", ScrollTrigger.update);
+
+// // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+// ScrollTrigger.scrollerProxy("#main", {
+//   scrollTop(value) {
+//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//   getBoundingClientRect() {
+//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//   },
+//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+// });
+
+
+
+// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+// ScrollTrigger.refresh();
+
+// }
+
+
+   
+
+
 
 
 
@@ -78,6 +95,7 @@ function reveal_Item(){
 }
 
 
+// --------------------------------------------------
 
 
 var tl = gsap.timeline();
@@ -145,7 +163,7 @@ tl.to(".parent .child",{
    ease:Circ.easeInOut,
 
 })
-
+// --------------------------------------------------
 
 // page1 
 
@@ -155,19 +173,56 @@ tl.to(".parent .child",{
 //    width:"100%",
 //    height:"98%",
 // });
-let t3 = gsap.timeline();
-t3.to(".about",{
-   scrollTrigger:{
-      trigger:".video",
-      start:"50% 40%",
-      end:"50% 50%",
-      markers:true,
-      scrub:1,
-      // pin:true,
+// let t3 = gsap.timeline();
+// t3.to("#page1",{
+//    scrollTrigger:{
+//       trigger:"#page1",
+//       start:"50% 40%",
+//       end:"50% 30%",
+//       markers:true,
+//       scrub:1,
+//       // pin:true,
+//    }
+// })
+
+// .to(".video",{
+//    top:"100%",
+//    duration:2,
+//    opacity:1,
+   
+
+   
+//  })
+let t3 = gsap.timeline({
+   // yes, we can add it to an entire timeline!
+   scrollTrigger: {
+     trigger: '.page1',
+   //   scroller:".page",
+     scrub:4,
+     start: '-50% 50%', 
+     end: '-50% 35%', 
+     markers:true,
+     pin:true,
+   //   duration:5,
    }
+ })
+.to(".video",{
+   
+   top:"50%",
+   height:"0%",
+   // ease:"cubic-bezier(.01,.53,.87,-1.09)",
+   
+   duration:2,
+
 })
-t3.to(".video video",{
-   y:100,
+.to(".about",{
+   // delay:1,
+   duration:6,
+   
+   // ease:"cubic-bezier(.01,.53,.87,-1.09)",
+   // scrub:5,
+   height:"100vh",
+   top:0,
 })
 // gsap.to(".about",{
 //    top:"-100%",
@@ -211,3 +266,14 @@ t3.to(".video video",{
 //    ease:Circ.easeInOut,
 //    duration:1,
 // })
+
+   var swiper = new Swiper(".right .mySwiper", {
+      slidesPerView: 1,
+      
+      loop: true,
+      pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      }
+   });
+   
